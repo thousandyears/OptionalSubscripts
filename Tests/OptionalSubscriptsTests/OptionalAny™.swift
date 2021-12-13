f1706ac86a61132f6b7ed21b6c87304a94cd7645
Milos <milos@screensailor.net>
2021-12-13 00:25:20 +0000

    test cleanup

--- Tests/OptionalSubscriptsTests/OptionalAny™.swift
+++ Tests/OptionalSubscriptsTests/OptionalAny™.swift
@@ -3,37 +3,6 @@
 //
 
 final class OptionalAny™: Hopes {
-    
-    func test() throws {
-        
-var 📦: Any?
-
-📦 = nil
-📦 = []
-📦 = [:]
-
-📦[] = "👋"
-try 📦[] == "👋"
-
-📦["one"] = 1
-try 📦["one"] == 1
-
-📦["one", 2] = 2
-try 📦["one", 2] == 2
-
-📦["one", 10] = nil
-try 📦["one"] as [Int?] == [nil, nil, 2]
-
-📦["one", 2] = ["three": 4]
-try 📦["one", 2, "three"] == 4
-
-📦["one", 2] = nil
-📦["one"] == nil
-
-📦["one", "two"] = nil
-📦[] == nil
-
-    }
 
     func test_subscript() throws {
 
